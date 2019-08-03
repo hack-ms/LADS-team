358adbab912a3063972310242d84dc769cfaf7f5
albertobazilio98 <albertobazilio98@gmail.com>
2019-08-03 19:12:09 -0400

    APP - Recebimento de imagens no front
    -implementação da função que recebe o array de rotas para receber as imagens

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <v-app>
-    <v-card>
+    <div>
 
       <v-navigation-drawer
+        v-model="drawer" 
+        absolute
+        cliped
         class="purple darken-4"
         dark
-        permanent
-        expand-on-hover
       >
-        <v-list
-          nav
-          dense
-        >
+        <v-list dense>
           <v-list-item link>
             <v-list-item-icon>
               <v-icon>mdi-folder</v-icon>
@@ -32,60 +30,99 @@
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
+
+      <v-toolbar color="purple darken-4" fixed>
+        <v-icon color="white" @click="drawer = !drawer">mdi-play</v-icon>
+        <v-toolbar-title class="title white--text ml-2">Pão de Batata</v-toolbar-title>
+      </v-toolbar>
       
-      <!-- <template> -->
-        <v-card
-          class="mx-auto"
-          max-width="400"
-        >
-          <v-img
-            src="@/assets/template-despesas.jpg"
+      <v-content>
+        <template>
+
+        <v-container fluid>
+          <v-layout justify-center>
+          <v-flex lg8 sm10 xs12>
+          <v-card
+            class="mx-auto"
+            max-width="400"
           >
-            <v-layout align-start justify-end>
-                <v-icon color="white">mdi-share</v-icon>
-            </v-layout>
-          </v-img>
-
-          <v-card-text>
-            <span>Number 10</span><br>
-            <span class="text--primary">
-              <span>Whitehaven Beach</span><br>
-              <span>Whitsunday Island, Whitsunday Islands</span>
-            </span>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn
-              text
-              color="orange"
+            <v-img
+              :src="img"
             >
-              Share
-            </v-btn>
-            <v-btn
-              text
-              color="orange"
-            >
-              Explore
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      <!-- </template> -->
+              <v-layout align-start justify-end>
+                  <v-icon color="white">mdi-share</v-icon>
+              </v-layout>
+            </v-img>
 
-    </v-card>
+            <v-card-text>
+              <span>Number 10</span><br>
+              <span class="text--primary">
+                <span>Whitehaven Beach</span><br>
+                <span>Whitsunday Island, Whitsunday Islands</span>
+              </span>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                text
+                color="orange"
+              >
+                Share
+              </v-btn>
+              <v-btn
+                text
+                color="orange"
+              >
+                Explore
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          </v-flex>
+          </v-layout>
+        </v-container>
+        
+        </template>
+      </v-content>
+    </div>
+
   </v-app>
 </template>
 
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import find from "@/service/service";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
   data: () => ({
+    src: "http://172.50.4.210:3333/",
+    img: "",
+    paths: [],
+    drawer: false
     //
   }),
+  computed: {
+    randImg() {
+      var i = parseInt(Math.random()*this.paths.length);
+      console.log(i)
+      return (i);
+    }
+  },
+  methods: {
+
+    async getPaths() {
+      try {
+        let response = await find("images");
+        console.log(response.data);
+        this.paths = response.data;
+      } catch {
+        console.log(e);
+      }
+      this.img = this.src + this.paths[this.randImg];
+    }
+  },
+  created() {
+    this.getPaths();
+  }
 };
 </script>
