3e819aeb0ac10422c33dc750875c3caec42b7f94
albertobazilio98 <albertobazilio98@gmail.com>
2019-08-03 20:41:52 -0400

    API - Updates
    - Despesas SEJUSP

--- api/src/controllers/despesas-controller.js
+++ api/src/controllers/despesas-controller.js
@@ -10,24 +10,50 @@ const read = async (req, res, next) => {
     let pattern = "Portal da Transparência do Estado de Mato Grosso do Sul";
 
     let wanted = ["Período", "SECRETARIA DE ESTADO DE JUST.SEGUR.PUBLICA"];
-    let text = `No %, o Governo Estadual se comprometeu em pagar ao órgão * uma quantia de R$!, e o repasse foi de R$.`;
+    let text = `No %, o Governo Estadual se comprometeu em pagar ao órgão * uma quantia de R$!, e o repasse foi de R$@`;
 
     let data = await csv().fromFile(csvpath);
-    // console.log(data);
+    console.log(data);
     for (let i in data) {
       if (data[i][pattern].startsWith(wanted[0])) {
-        text = text.replace("%", data[i][pattern]); // replace  Periodo...
+        text = text.replace("%", "ano de " + data[i][pattern].slice(-4)); // replace  Periodo...
       } else if (data[i][pattern].startsWith(wanted[1])) {
+        wanted[1] = "SEJUSP";
         // data[i][pattern] = data[i][pattern].split(" ");
         text = text.replace("*", wanted[1]); // replace SECRETARIA...
-        text = text.replace("!", data[i][pattern]["field2"]); // replace R$
-        console.log(text);
+        text = text.replace(
+          "!",
+          data[i]["field2"].slice(data[i]["field2"].indexOf(";") + 2) +
+            "," +
+            data[i]["field3"].slice(0, data[i]["field3"].indexOf(";") - 1)
+        ); // replace R$
+        /*console.log(
+          data[i]["field3"].slice(data[i]["field3"].indexOf(";") + 2) +
+            "," +
+            data[i]["field4"]
+        );*/
+        text = text.replace(
+          "@",
+          data[i]["field3"].slice(data[i]["field3"].indexOf(";") + 2) +
+            "," +
+            data[i]["field4"].slice(0, -1)
+        );
+        //console.log(text);
       }
     }
 
     // let a = await img.create();
     // console.log(a);
-    return res.json({}); //res.sendFile(path.resolve(await img.create(wanted, text)));
+    return res.sendFile(
+      path.resolve(
+        await img.create(
+          "despesas",
+          "template02.png",
+          wanted[1].split(" "),
+          text
+        )
+      )
+    ); //res.sendFile(path.resolve(await img.create(wanted, text)));
   } catch (error) {
     next(error);
   }
